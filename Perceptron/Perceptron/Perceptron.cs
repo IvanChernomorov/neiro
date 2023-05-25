e087c7306d4030cf6bfb36f077f1a7ea721dca24
Ivan Chernomorov <90576997+IvanChernomorov@users.noreply.github.com>
2023-05-25 11:20:13 +0300

    Залил перцептрон

---
+++ Perceptron/Perceptron/Perceptron.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    internal class Perceptron
+    {
+        private int inputSize;
+        private double learningRate;
+        int maxEpoch;
+        private double[] weights;
+
+        public Perceptron(int inputSize, double learningRate = 0.1, int maxEpoch = 100)
+        {
+            this.inputSize = inputSize;
+            this.learningRate = learningRate;
+            this.maxEpoch = maxEpoch;
+            this.weights = new double[inputSize + 1];
+        } 
+
+        public double Predict(double[] inputs)
+        {
+            double sum = 0;
+            for (int i = 0; i < inputs.Length; i++)
+                sum += weights[i] * inputs[i];
+            sum += weights[weights.Length - 1];
+            return sum >= 0 ? 1 : 0;
+        }
+
+        public void Train(double[][] inputs, double[] target)
+        {
+            for(int i = 0; i < maxEpoch; i++)
+            {
+                for(int j = 0; j < inputs.Length; j++)
+                {
+                    double predict = Predict(inputs[j]);
+                    double error = target[j] - predict;
+                    for (int k = 0; k < inputs[j].Length; k++)
+                        weights[k] += learningRate * error * inputs[j][k];
+                    weights[weights.Length-1] += learningRate * error;
+                }
+            }
+        }
+    }
+}
---
+++ Perceptron/Perceptron/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] trainingData = new double[4][];
+            trainingData[0] = new[] { 0.0, 0.0 };
+            trainingData[1] = new[] { 0.0, 1.0 };
+            trainingData[2] = new[] { 1.0, 0.0 };
+            trainingData[3] = new[] { 1.0, 1.0 };
+
+            double[] labels = new[] { 0.0, 0.0, 0.0, 1.0 };
+
+            Perceptron p = new Perceptron(2);
+            p.Train(trainingData, labels);
+
+            foreach(var input in trainingData)
+            {
+                double prediction = p.Predict(input);
+                Console.Write("Входные данные: ");
+                for(int i = 0; i < input.Length; i++)
+                    Console.Write(input[i] + " ");
+                Console.WriteLine($"\nПредикт: {prediction}");
+            }    
+            Console.ReadKey();
+        }
+    }
+}
