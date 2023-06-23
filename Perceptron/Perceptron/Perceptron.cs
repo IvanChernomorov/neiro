5a165f4bb9b9a0dd55f6bc3dba878e7664ebcce3
Ivan Chernomorov <90576997+IvanChernomorov@users.noreply.github.com>
2023-06-23 20:27:39 +0300

    Залил исправленный перцептрон

---
+++ Perceptron/Perceptron/Perceptron.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Perceptron
+{
+    public class Perceptron
+    {
+        private int inputSize;
+        private int hiddenSize;
+        private double learningRate;
+        private int maxEpoch;
+        private double[,] inputWeights;
+        private double[] hiddenWeights;
+
+        public Perceptron(int inputSize, int hiddenSize, double learningRate = 0.1, int maxEpoch = 100)
+        {
+            Random rand = new Random();
+            inputWeights = new double[hiddenSize, inputSize];
+            for(int i = 0; i < hiddenSize; i++)
+                for(int j = 0; j < inputSize; j++)
+                    inputWeights[i, j] = rand.NextDouble() * 2 - 1;
+
+            hiddenWeights = new double[hiddenSize];
+            for (int i = 0; i < hiddenSize; i++)
+                hiddenWeights[i] = rand.NextDouble() * 2 - 1;
+
+            this.learningRate = learningRate;
+            this.maxEpoch = maxEpoch;
+            this.hiddenSize = hiddenSize;
+            this.inputSize = inputSize;
+        }
+
+        public double F(double x)
+        {
+            return (Math.Exp(x) - Math.Exp(-x)) / (Math.Exp(x) + Math.Exp(-x));
+        }
+
+        public double DF(double x)
+        {
+            return 1 - Math.Pow(F(x), 2);
+        }
+
+        public double[] Predict(double[] input)
+        {
+            double[] sum = new double[hiddenSize];
+
+            for (int i = 0; i < hiddenSize; i++)
+            {
+                for (int j = 0; j < inputSize; j++)
+                    sum[i] += inputWeights[i, j] * input[j];
+                sum[i] = F(sum[i]);
+            }
+
+            double res = 0.0;
+            for (int i = 0; i < hiddenSize; i++)
+                res += hiddenWeights[i] * sum[i];
+            res = F(res);
+
+            double[] out1 = new double[hiddenSize + 1];
+            out1[0] = res;
+            for (int i = 1; i < hiddenSize + 1; i++)
+                out1[i] = sum[i - 1];
+
+            return out1;
+        }
+
+        public void Train(double[][] inputs, double[] targets)
+        {
+            Random rand = new Random();
+
+            for (int k = 0; k < maxEpoch; k++)
+            {
+                int randInput = rand.Next(0, inputs.Length);
+
+                double[] x = inputs[randInput];
+                double[] result = Predict(x);
+
+                double delta = (result[0] - targets[randInput]) * DF(result[0]);
+               
+                for(int i = 0; i < hiddenSize; i++)
+                    hiddenWeights[i] -= learningRate * delta * result[i+1];
+
+                double[] delta2 = new double[hiddenSize];
+                for (int i = 0; i < hiddenSize; i++)
+                    delta2[i] = hiddenWeights[i] * delta * DF(result[i + 1]);
+
+                for (int i = 0; i < hiddenSize; i++)
+                    for (int j = 0; j < inputSize; j++)
+                        inputWeights[i, j] -= x[j] * delta2[i] * learningRate;
+            }
+        }
+    }
+
+}
---
+++ Perceptron/Perceptron/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Perceptron perceptron = new Perceptron(3, 10, 0.1, 1000);
+
+            double[][] trainData = new double[][]
+            {
+            new double[] { -1, -1, -1},
+            new double[] { -1, -1, 1 },
+            new double[] { -1, 1, -1 },
+            new double[] { -1, 1, 1 },
+            new double[] { 1, -1, -1 },
+            new double[] { 1, -1, 1 },
+            new double[] { 1, 1, -1 },
+            new double[] { 1, 1, 1 }
+            };
+
+            double[] targets = new[] { -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 };
+
+            perceptron.Train(trainData, targets);
+
+            for(int i = 0; i < trainData.GetLength(0); i++)
+            {
+                double result = perceptron.Predict(trainData[i])[0];
+                Console.WriteLine($"Выходное значение НС: {result} => {targets[i]}");
+            }
+
+            //Console.WriteLine($"Оптимальные веса: {perceptron.W1[0, 0]}, {perceptron.W1[0, 1]}, {perceptron.W1[0, 2]}; {perceptron.W1[1, 0]}, {perceptron.W1[1, 1]}, {perceptron.W1[1, 2]}; {perceptron.W2[0]}, {perceptron.W2[1]}");
+            Console.ReadKey();
+        }
+    }
+}
